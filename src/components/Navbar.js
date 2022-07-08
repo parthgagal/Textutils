137a96d94aa8ab6108f8f226d8ce16f700c5cd33
Parth Gagal <parthgagal999@gmail.com>
2022-07-08 12:19:47 +0530

    space

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,6 +30,7 @@ export default function Navbar(props) {
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
             <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+            
           </div>
         </div>
       </div>
