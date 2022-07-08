dfa1e3167df0896946cf9d05f24375cb2fa5cbad
Parth Gagal <parthgagal999@gmail.com>
2022-07-08 12:00:14 +0530

    new navbar item

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="/">About</a>
             </li>
             {/* <li className="nav-item">
               <a className="nav-link" href="/about">{props.aboutText}</a>
